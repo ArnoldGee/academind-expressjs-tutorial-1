0892e09f84d36edff4ccf9560f4249630007b72c
Arnau <agomez1f@gmail.com>
2020-10-25 08:35:09 +0100

    Served static files like html and css

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -7,13 +11,13 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use( "/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=> {
-  res.status(404).send('<h1>Boo ho 404 please come no more </h1>')
-})
-
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path')
+
 const express = require('express')
 const router = express.Router();
 
+const rootDir = require('../util/path')
+
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST" ><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post('/product', (req, res, next) => {
